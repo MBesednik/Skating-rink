b5dd8f1663fb2708f51417b755913cad1099b9d6
Mateo Besednik <mateo.besednik@gmail.com>
2022-10-17 23:31:29 +0200

    added swiper

---
+++ script.js
@@ -0,0 +1,40 @@
+//Swiper slider
+var swiper = new Swiper('.bg-slider-thumbs', {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+var swiper2 = new Swiper('.bg-slider', {
+  loop: true,
+  spaceBetween: 0,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//Javacript for the scroll indicator bar
+window.addEventListener('scroll', () => {
+  const indicatorBar = document.querySelector('.scroll-indicator-bar');
+
+  const pageScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const scrollValue = (pageScroll / height) * 100;
+
+  indicatorBar.style.width = scrollValue + '%';
+});
+
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector('.nav-menu-btn');
+const closeBtn = document.querySelector('.nav-close-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navigation.classList.remove('active');
+});
