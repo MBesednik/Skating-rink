5bd7f5bedd4b5d7f477ea4d611773dc05ff0a620
Mateo Besednik <mateo.besednik@gmail.com>
2022-10-20 17:14:40 +0200

    created card swiper

--- script.js
+++ script.js
@@ -60,3 +60,18 @@ window.addEventListener('scroll', function () {
   //   topLink.classList.remove('show-link');
   // }
 });
+
+// Cards slider
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  },
+});
