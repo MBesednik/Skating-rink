247ad7158492d266e90a604f0d37af648fda5f8f
Mateo Besednik <mateo.besednik@gmail.com>
2022-11-16 22:55:50 +0100

    added gallery and filter rink card

--- pages/rink/rinkScript.js
+++ pages/rink/rinkScript.js
@@ -69,3 +69,76 @@ window.addEventListener('scroll', function () {
 if (window.location.href.indexOf('rink.html') > -1) {
   navbar.classList.add('fixed-nav');
 }
+
+// ********** Filtriranje klizališta **********
+// get parent element
+const sectionCenter = document.querySelector('.section-rink');
+const btnContainer = document.querySelector('.btn-container');
+// display all items when pdate loads
+window.addEventListener('DOMContentLoaded', function () {
+  diplayMenuItems(rinkCard);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (rink) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${rink.img} alt=${rink.title} class="photo" />
+          <div class="rink-info">
+            <div class="rink-title">
+              <h4>${rink.title}</h4>
+              <h4>${rink.year}</h4>
+            </div>
+            <p class="item-text">
+              ${rink.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = rinkCard.reduce(
+    function (values, item) {
+      if (!values.includes(item.year)) {
+        values.push(item.year);
+      }
+      return values;
+    },
+    ['klizališta']
+  );
+  const yearBtns = categories
+    .map(function (year) {
+      return `<button type="button" class="filter-btn" data-id=${year}>
+          ${year}
+        </button>`;
+    })
+    .join('');
+
+  btnContainer.innerHTML = yearBtns;
+  const filterBtns = btnContainer.querySelectorAll('.filter-btn');
+  console.log(filterBtns);
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      // console.log(e.currentTarget.dataset);
+      const year = e.currentTarget.dataset.id;
+      const yearBtn = rinkCard.filter(function (menuItem) {
+        // console.log(menuItem.year);
+        if (menuItem.year === year) {
+          return menuItem;
+        }
+      });
+      if (year === 'klizališta') {
+        diplayMenuItems(rinkCard);
+      } else {
+        diplayMenuItems(yearBtn);
+      }
+    });
+  });
+}
